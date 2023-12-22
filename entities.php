9718b3d3ca9e1843dbb8de0bb68e446c4b2f9286
Tanguy Decabrat <103762138+TDecabrat@users.noreply.github.com>
2023-12-22 17:32:14 +0100

    fix: Ajout de la documentation et gestion de l'image non existante
    Ajout d'image par défaut

--- entities.php
+++ entities.php
@@ -1,28 +1,56 @@
 <?php
 
+/**
+ * Classe définissant les joueurs
+ * 
+ * @param string $color Couleur du joueur
+ * @param array $tokens Jetons du joueur
+ * param int $score Score du joueur | Possiblement ajoutable
+ */
 class Player 
 {
     public string $color;
     public array $tokens = [];
     // public int $score; // Additional feature
 
+    /**
+     * @param string $color Couleur du joueur
+     */
     public function __construct(string $color) {
         $this->color = $color;
         $this->tokens = [];
     }
 }
 
+/**
+ * Classe définissant les tokens des joueurs
+ * 
+ * @param Player $player Joueur auquel appartient le jeton
+ * @param int $pos_x Position en x du jeton dans le plateau
+ * @param int $pos_y Position en y du jeton dans le plateau
+ * @param string $image_path Chemin vers l'image du jeton | Si non existante, charge l'image par défaut
+ */
 class Token {
     public Player $player;
     public int $pos_x;
     public int $pos_y;
-    public string $image_path;
+    public string $image_path; // Image à charger
 
+    /**
+     * @param Player $player Joueur auquel appartient le jeton
+     * @param int $pos_x Position en x du jeton
+     * @param int $pos_y Position en y du jeton
+     * @param string $image_path Chemin vers l'image du jeton
+     */
     public function __construct(Player $player, int $pos_x, int $pos_y, string $image_path) {
         $this->player = $player;
         $this->pos_x = $pos_x;
         $this->pos_y = $pos_y;
-        $this->image_path = $image_path;
+        if (file_exists($image_path)) {
+            $this->image_path = $image_path;
+        } else {
+            $this->image_path = "sprites/defaultToken.png";
+        }
     }
 }
 ?>
--- index.php
+++ index.php
@@ -3,16 +3,21 @@ require_once("entities.php");
 
 $player1 = new Player("Red");
 $player2 = new Player("Yellow");
+$player3 = new Player("Blue");
 
 $token1 = new Token($player1, 0, 0, "sprites/".$player1->color."Token.png");
 $token2 = new Token($player2, 0, 0, "sprites/".$player2->color."Token.png");
+$token3 = new Token($player3, 0, 0, "sprites/".$player3->color."Token.png");
 
 $_SESSION["player1"] = $player1;
 $_SESSION["player2"] = $player2;
+$_SESSION["player3"] = $player3;
 
 $player1->tokens[] = $token1;
 
 echo "HomePage";
 echo "<img src='".$token1->image_path."'>";
 echo "<img src='".$token2->image_path."'>";
+echo "<img src='".$token3->image_path."'>";
+
 
